800c911d1101e44ac359ef457486d163be4c58d6
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-12-28 12:10:08 +0100

    Avancement : - Version 1 terminée avec affichage correct - Je vais essayer d'améliorer l'affichage

--- src/elevator_v1.c
+++ src/elevator_v1.c
@@ -4,10 +4,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <fcntl.h>      // Nécessaire pour O_CREAT sur Mac
+
 #include "elevator.h"
 #include "common.h"
 
-// --- VARIABLES GLOBALES (Comme dans le Listing 6 du cours) ---
+// VARIABLES GLOBALES (Comme dans le Listing 6 du cours)
 Passager buffer;        // La donnée partagée (taille 1)
 pthread_mutex_t mutex;  // Pour protéger l'accès au buffer [cite: 469]
 sem_t *empty;           // Sémaphore : places libres 
@@ -31,53 +32,84 @@ void *thread_ascenseur(void *arg) {
 
         // On attend un passager
         sem_wait(full); 
-        // On reccupere le passage
+        // On reccupere le passager
         pthread_mutex_lock(&mutex); 
         Passager p = buffer; 
         pthread_mutex_unlock(&mutex);
 
-        // On fait le trajet
-        printf("[ASCENSEUR] Prise en charge client %d (Etage %d -> %d)\n", p.id, p.etage_depart, p.etage_destination);
+        // On fait le trajet vers le passager
+        if (etage_actuel != p.etage_depart) {
+            printf("[ASCENSEUR] En route vers l'étage %d pour prendre en charge le passager %d.\n", p.etage_depart, p.id);
+            for (int i=0; i<abs(p.etage_depart - etage_actuel); i++) {
+                printf("[ASCENSEUR] ...\n");
+                sleep(1);
+            }
+            etage_actuel = p.etage_depart;
+        }
+
+        printf("[ASCENSEUR] Prise en charge passager %d (Etage %d -> %d)\n", p.id, etage_actuel, p.etage_destination);
         sleep(2);         
-        etage_actuel = p.etage_destination;
-        printf("[ASCENSEUR] Client %d déposé à l'étage %d.\n", p.id, etage_actuel);
+
+
+        // On fait le trajet vers la destination du passager
+        if (etage_actuel != p.etage_destination) {
+            printf("[ASCENSEUR] En route vers l'étage %d pour déposer le passager %d.\n", p.etage_destination, p.id);
+            for (int i=0; i<abs(p.etage_destination - etage_actuel); i++) {
+                printf("[ASCENSEUR] ...\n");
+                sleep(1);
+            }
+            etage_actuel = p.etage_destination;
+        }
+
+        printf("[ASCENSEUR] Passager %d déposé à l'étage %d.\n", p.id, etage_actuel);
 
         // On libere l'ascenseur
-        sem_post(empty); // [cite: 507]
+        sem_post(empty);
     }
     return NULL;
 }
 
-// --- LE PRODUCTEUR (L'Usager) ---
+/**
+ * Producteur : L'Usager
+ * Chaque usager crée une requête pour aller d'un étage à un autre.
+ */
 void *thread_usager(void *arg) {
     Passager moi = *(Passager *)arg;
 
-    printf("[CLIENT %d] Je veux aller de %d à %d.\n", moi.id, moi.etage_depart, moi.etage_destination);
+    printf("[USAGER %d] Je veux aller de %d à %d.\n", moi.id, moi.etage_depart, moi.etage_destination);
 
-    // 1. Attendre que l'ascenseur soit libre (P)
-    sem_wait(empty); // [cite: 472]
+    // Si le client est déjà à l'étage demandé.
+    if (moi.etage_depart == moi.etage_destination) {
+        printf("[USAGER %d] Demande annulée. Je suis déjà à l'étage %d, pas besoin d'ascenseur.\n", moi.id, moi.etage_depart);
+        return NULL;
+    }
 
-    // 2. Protéger l'écriture
-    pthread_mutex_lock(&mutex); // [cite: 473]
-    buffer = moi; // Je rentre dans l'ascenseur (j'écris ma demande)
-    pthread_mutex_unlock(&mutex); // [cite: 478]
+    // On Attend que l'ascenseur soit libre (P)
+    sem_wait(empty); 
 
-    // 3. Signaler à l'ascenseur qu'il y a un client (V)
-    sem_post(full); // [cite: 479]
+    // Lorsque l'ascenseur est libre, on fait notre requête
+    pthread_mutex_lock(&mutex); 
+    buffer = moi;
+    pthread_mutex_unlock(&mutex); 
 
-    printf("[CLIENT %d] Je suis dans l'ascenseur.\n", moi.id);
+    // On prévient que l'ascenseur est plein (V)
+    sem_post(full); 
+
+    printf("[USAGER %d] C'est moi le prochain !\n", moi.id);
     return NULL;
 }
 
-// --- INITIALISATION ---
+/**
+ * Lance la simulation V1 : Ascenseur capacité 1 personne
+ */
 void run_elevator_v1(int nb_usagers) {
     printf("=== Lancement V1 (Strict Producteur-Consommateur) ===\n");
 
     pthread_t ascenseur;
-    pthread_t threads_usagers[nb_usagers];
-    Passager donnees_usagers[nb_usagers]; // Tableau pour stocker les infos
+    pthread_t threads_usagers[nb_usagers];   // Tableau des threads usagers
+    Passager donnees_usagers[nb_usagers];    // Tableau pour stocker les infos
 
-    // Initialisation Mutex [cite: 527]
+    // Initialisation Mutex
     pthread_mutex_init(&mutex, NULL);
 
     // Initialisation Sémaphores (Version Mac obligée)
@@ -88,24 +120,24 @@ void run_elevator_v1(int nb_usagers) {
     empty = sem_open("/sem_v1_empty", O_CREAT, 0644, 1);
     full  = sem_open("/sem_v1_full",  O_CREAT, 0644, 0);
 
-    // Création thread Ascenseur [cite: 536]
+    // Création thread Ascenseur
     pthread_create(&ascenseur, NULL, thread_ascenseur, NULL);
 
-    // Création threads Usagers [cite: 533]
+    // Création threads Usagers
     for (int i = 0; i < nb_usagers; i++) {
         donnees_usagers[i].id = i;
         donnees_usagers[i].etage_depart = rand() % 5;
         donnees_usagers[i].etage_destination = rand() % 5;
         pthread_create(&threads_usagers[i], NULL, thread_usager, &donnees_usagers[i]);
-        sleep(1); // Petit délai pour qu'ils n'arrivent pas tous à la milliseconde exacte
+        sleep(1);
     }
 
-    // Attente fin des usagers [cite: 543]
+    // Attendre que tous les threads se terminent
     for (int i = 0; i < nb_usagers; i++) {
         pthread_join(threads_usagers[i], NULL);
     }
 
-    // Nettoyage [cite: 554]
+    // Fermeture et destruction des sémaphores et mutex
     sem_close(empty);
     sem_close(full);
     sem_unlink("/sem_v1_empty");
@@ -113,5 +145,4 @@ void run_elevator_v1(int nb_usagers) {
     pthread_mutex_destroy(&mutex);
     
     printf("=== Fin simulation ===\n");
-    // On laisse le thread ascenseur mourir avec le programme (car boucle infinie)
 }
